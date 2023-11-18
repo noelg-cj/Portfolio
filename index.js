65b89af0f72bc62283b0e44eace968e0bfe6c3a6
Noel George C J <noelgeorge2004@gmail.com>
2023-11-18 19:06:58 +0530

    Added finishing touches to structure

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-let letters = document.querySelectorAll('.scroll-text')
+let letters = document.querySelector('.scroll-text');
+let angle = 360/letters.innerText.length;
+letters.innerHTML = letters.innerText.split("").map((letter, index) => {
+    return `<span style="transform: rotate(${index * angle}deg)">${letter}</span>`
+}).join("");
